613ee93b4d5a3d843ac6ea9fd9f991c9af88963d
Smartpro94 <matthieu.barilone@gmail.com>
2024-12-05 15:26:12 +0100

    bugs

--- ArgentBank-Frontend-main/argent-bank/src/redux/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState, AppDispatch } from './store';
-
-//Hook pour dispatcher des actions
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-//Hook pour accéder à l'état global du store
-export const useAppSelector = <T>(selector: (state: RootState) => T): T =>
-  useSelector(selector);
